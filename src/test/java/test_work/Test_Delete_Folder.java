b2ced501aa16a8ef6f694ec9ad98618271d3065d
Dron73 <askvortsov94@gmail.com>
2017-04-03 00:37:48 +0400

    Update Test_Delete_Folder.java

--- src/test/java/test_work/Test_Delete_Folder.java
+++ src/test/java/test_work/Test_Delete_Folder.java
@@ -19,7 +19,7 @@ public class Test_Delete_Folder {
                     .then()
                     .extract().response();
             if (response.getStatusCode() == 204) {
-                System.out.println("Папка "+ nameFolder + " успешно удалена" + response.getStatusCode());
+                System.out.println("Папка "+ nameFolder + " успешно удалена");
             } else if (response.getStatusCode() == 404) {
                 System.out.println("Папка " + nameFolder + " не найдена!");
             }
