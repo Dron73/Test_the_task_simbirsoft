ee5665e9b432e1046e3012ddd48d2f4cb7bb0639
Andrey Skvortcov <askvortsov94@gmail.com>
2017-04-06 00:53:28 +0400

    Update.

--- src/test/java/test_work/Test_Create_Folder.java
+++ src/test/java/test_work/Test_Create_Folder.java
@@ -1,48 +1,86 @@
 package test_work;
 
 import com.jayway.restassured.path.json.JsonPath;
-import com.jayway.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class Test_Create_Folder {
-    @Test
-    public void testCreateFolder() {
-        String nameFolder = "000";
-        StringBuilder stringBuilder = new StringBuilder();
-        System.out.println("Тест на создание:");
-        try {
-            Response response = given()
-                    .contentType("application/json; charset=utf-8")
-                    .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
-                    .when()
-                    .put("https://cloud-api.yandex.net/v1/disk/resources?path=" + nameFolder)
-                    .then().extract().response();
 
-            JsonPath jsonPath = new JsonPath(response.asString());
-            int codeStatus = response.getStatusCode();
-            if (codeStatus == 201) {
-                String href = jsonPath.get("href").toString();
-                String method = jsonPath.get("method").toString();
-                String templated = jsonPath.get("templated").toString();
-                stringBuilder.append("Href: " + href + "\n"
-                        + "Method: " + method + "\n"
-                        + "Templated: " + templated + "\n"
-                        + "CodeStatus status: " + codeStatus + "\n");
-                System.out.println(stringBuilder.toString());
-            } else{
-                String message = jsonPath.get("message").toString();
-                String description = jsonPath.get("description").toString();
-                String error = jsonPath.get("error").toString();
-                stringBuilder.append("Message: " + message + ".\n"
-                        + "Description: " + description + ".\n"
-                        + "Error: " + error + ".\n");
-                System.out.print(stringBuilder);
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void testCreateFolder201() {
+        String nameFolder = "Тест201"; // OK
+
+        JsonPath responseJsonPath = given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder)
+                .then()
+                .assertThat().statusCode(201).body("method", equalTo("GET"))
+                .extract().jsonPath();
+    }
+
+    @Test
+    public void testCreateFolder400() {
+        String nameFolder = ""; // Некорректные данные. (без имени)
+
+        JsonPath responseJsonPath = given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder)
+                .then()
+                .assertThat().statusCode(400).body("error", equalTo("FieldValidationError"))
+                .extract().jsonPath();
+    }
+
+    @Test
+    public void testCreateFolder401() {
+        String nameFolder = "Тест401"; // Не авторизован.
+
+        JsonPath responseJsonPath = given()
+                .contentType("application/json; charset=utf-8")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder)
+                .then()
+                .assertThat().statusCode(401).body("error", equalTo("UnauthorizedError"))
+                .extract().jsonPath();
+    }
+
+    @Test
+    public void testCreateFolder404() {
+        String nameFolder = "Тест404"; // Не удалось найти запрошенный ресурс.
+
+        JsonPath responseJsonPath = given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resourcessss?path=" + nameFolder)
+                .then()
+                .assertThat().statusCode(404).body("error", equalTo("NotFoundError"))
+                .extract().jsonPath();
+    }
+
+    @Test
+    public void testCreateFolder409() {
+        String nameFolder = "Тест409"; // Попытка создать уже созданную папку
+
+        given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder);
+
+        JsonPath responseJsonPath = given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder)
+                .then()
+                .assertThat().statusCode(409).body("error", equalTo("DiskPathPointsToExistentDirectoryError"))
+                .extract().jsonPath();
     }
 }
 
--- src/test/java/test_work/Test_Delete_Folder.java
+++ src/test/java/test_work/Test_Delete_Folder.java
@@ -1,42 +1,109 @@
 package test_work;
 
 import com.jayway.restassured.path.json.JsonPath;
-import com.jayway.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 
 public class Test_Delete_Folder {
-    @Test
-    public void testDeleteFolder() {
-        String nameFolder = "000";
-        StringBuilder stringBuilder = new StringBuilder();
-        System.out.println("Тест на удаление:");
-        try {
-            Response response = given()
-                    .contentType("application/json")
-                    .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
-                    .when()
-                    .delete("https://cloud-api.yandex.net:443/v1/disk/resources?path="
-                            + nameFolder + "&permanently=false")
-                    .then().extract().response();
 
-            JsonPath jsonPath = new JsonPath(response.asString());
-            int codeStatus = response.getStatusCode();
-            if (codeStatus == 204) {
-                System.out.println("Папка " + nameFolder + " успешно удалена"
-                        + "\n" + "CodeStatus: " + codeStatus);
-            } else {
-                String message = jsonPath.get("message").toString();
-                String description = jsonPath.get("description").toString();
-                String error = jsonPath.get("error").toString();
-                stringBuilder.append("Message: " + message + ".\n"
-                        + "Description: " + description + ".\n"
-                        + "Error: " + error + ".\n");
-                System.out.println(stringBuilder);
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void testDeleteFolder204() {
+        String nameFolder = "004"; //  	OK
+
+        given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder);
+
+        JsonPath response = given()
+                .contentType("application/json")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .delete("https://cloud-api.yandex.net:443/v1/disk/resources?path="
+                        + nameFolder + "&permanently=false")
+                .then().statusCode(204)
+                .extract().jsonPath();
     }
+
+    @Test
+    public void testDeleteFolder400() { // Некорректные данные.
+        String nameFolder = "delete400";
+
+        given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder);
+
+        JsonPath responseJsonPath = given()
+                .contentType("application/json")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .delete("https://cloud-api.yandex.net:443/v1/disk/resources?path="
+                        + "&permanently=false")
+                .then()
+                .assertThat().statusCode(400)
+                .body("error", equalTo("FieldValidationError"))
+                .and()
+                .body("description", equalTo("Error validating field \"path\": This field is required."))
+                .and()
+                .body("message", equalTo("Ошибка проверки поля \"path\": Это поле является обязательным."))
+                .extract().jsonPath();
+    }
+
+    @Test
+    public void testDeleteFolder401() { // Не авторизован.
+        String nameFolder = "delete401";
+
+        given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder);
+
+        JsonPath responseJsonPath = given()
+                .contentType("application/json")
+                .when()
+                .delete("https://cloud-api.yandex.net:443/v1/disk/resources?path="
+                        + "&permanently=false")
+                .then()
+                .assertThat().statusCode(401)
+                .body("description", equalTo("Unauthorized"))
+                .and()
+                .body("message", equalTo("Не авторизован."))
+                .and()
+                .body("error", equalTo("UnauthorizedError"))
+                .extract().jsonPath();
+    }
+
+    @Test
+    public void testDeleteFolder404() { // Не удалось найти запрошенный ресурс.
+        String nameFolder = "delete404";
+
+        given()
+                .contentType("application/json; charset=utf-8")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .put("https://cloud-api.yandex.net:443/v1/disk/resources?path=" + nameFolder);
+
+        JsonPath responseJsonPath = given()
+                .contentType("application/json")
+                .headers("Authorization", "OAuth AQAAAAAaz8MGAADLW2OZywZUMEcHtQH8YoPcLb4")
+                .when()
+                .delete("https://cloud-api.yandex.net:443/v1/disk/resources?path="
+                        + nameFolder + "fail" + "&permanently=false")
+                .then()
+                .assertThat().statusCode(404)
+                .body("description", equalTo("Resource not found."))
+                .and()
+                .body("message", equalTo("Не удалось найти запрошенный ресурс."))
+                .and()
+                .body("error", equalTo("DiskNotFoundError"))
+                .extract().jsonPath();
+    }
+
 }
